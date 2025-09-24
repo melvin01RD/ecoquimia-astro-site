bf5b3baefc1eb3a0bafac4c76cb784a3398d6232
melvin <melvin01rd@gmail.com>
2025-09-24 10:32:28 -0400

    homepage con hero + cotiza + servicios; API cotización

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,15 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: "https://ecoquimia.com.do"
 });
+
+
+ import { defineConfig } from 'astro/config';
+ import tailwind from '@astrojs/tailwind';
+ import { defineConfig } from 'astro/config';
+ import tailwind from '@astrojs/tailwind';
+ import sitemap from '@astrojs/sitemap';
 
 
