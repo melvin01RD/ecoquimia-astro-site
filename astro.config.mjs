bfb0f1252b038389c4679eb44227c9513db6c874
melvin <melvin01rd@gmail.com>
2025-09-28 00:06:57 -0400

    fix: actualizar config para Node.js 20 y adapter serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel"; // <- antes: "@astrojs/vercel/serverless"
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   site: "https://ecoquimia-astro-site.vercel.app",
