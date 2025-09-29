863f3f4459189a86d35b3d2dd367850df4923509
melvin <melvin01rd@gmail.com>
2025-09-29 00:26:15 -0400

    prerender servicios/nosotros

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // astro.config.mjs
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import vercel from '@astrojs/vercel' // <-- no uses '/serverless'
+import vercel from '@astrojs/vercel' 
 
 export default {
   site: 'https://ecoquimia-astro-site.vercel.app',
   output: 'server',
   integrations: [tailwind(), sitemap()],
-  adapter: vercel({ mode: 'serverless' }),
 }
