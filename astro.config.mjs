154e0ab31c1776363db496d162f59776c321ca94
melvin <melvin01rd@gmail.com>
2025-09-29 00:17:08 -0400

    feat(ui): layout limpio + header sólido + contacto responsive; estilos globales y logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // astro.config.mjs
-import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel'        // <— adapter correcto
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel' // <-- no uses '/serverless'
 
-export default defineConfig({
-  site: 'https://ecoquimia.com.do',         // o tu URL de vercel
-  adapter: vercel(),                        // <— clave para Vercel
+export default {
+  site: 'https://ecoquimia-astro-site.vercel.app',
+  output: 'server',
   integrations: [tailwind(), sitemap()],
-  // Si quieres SSR por defecto (no obligatorio):
-  // output: 'server',
-})
+  adapter: vercel({ mode: 'serverless' }),
+}
