ae97c1878398ce5b33d8f26af69b79b22d7d910a
melvin01RD <melvin01rd@gmail.com>
2026-01-05 15:18:11 -0400

    "seo: reemplaza sitemap automático por sitemap SSR manual"

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
-  site: 'https://ecoquimia-astro-site.vercel.app',
-  output: 'server',        // necesitas server para acciones/envío de formularios
-  adapter: vercel(),       // <-- importantísimo: usa el adapter de Vercel
-  integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+  site: 'https://www.ecoquimia.com.do',
+
+  output: 'server',
+
+  adapter: vercel(),
+
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+  ],
 })
